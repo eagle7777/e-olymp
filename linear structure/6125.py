7d4f488b0c368ab4870f2f7656b49b5b1ed79830
Valentyn <valentyn.kofanov@gmail.com>
2018-11-26 13:44:06 +0200

    refactoring

--- linear structure/6125.py
+++ linear structure/6125.py
@@ -4,24 +4,48 @@
 # source: https://www.e-olymp.com/ru/problems/6125
 # Created: 11/26/18
 
+# реализация очереди
+
 
 class Node:
+    """
+    вспомогательный класс
+    описывает узел (элемент) очереди
+    """
     def __init__(self, item=None):
-        self.item = item
-        self.next = None
+        """
+        конструктор
+        :param item:
+        """
+        self.item = item  # значение
+        self.next = None  # ссылка на следующий узел очереди
 
 
 class Queue:
+    """
+    класс очередь
+    """
     def __init__(self):
-        self.m_front = None
-        self.m_back = None
-        self.size = 0
+        """
+        конструктор
+        """
+        self.m_front = None  # будем запоминать первый узел,
+        self.m_back = None  # последний
+        self.size = 0  # и длину очереди (кол-во элементов в ней0
 
     def empty(self):
+        """
+        :return: True - елси очередь пустая / False - в противном случае
+        """
         return self.m_front is None and self.m_back is None
 
     def push(self, item):
-        self.size += 1
+        """
+        Добавить элемент в очередь
+        :param item: элемент
+        :return:
+        """
+        self.size += 1  # увеличиваем длину на 1
         new_node = Node(item)
         if self.empty():
             self.m_front = new_node
@@ -30,6 +54,10 @@ class Queue:
         self.m_back = new_node
 
     def pop(self):
+        """
+        Извлечь элемент из очереди
+        :return: первый элемент в очереди
+        """
         assert not self.empty(), 'Queue is empty!'
         current_front = self.m_front
         item = current_front.item
@@ -41,8 +69,39 @@ class Queue:
         return item
 
     def front(self):
+        """
+        посмотреть первый элемент, не удаляя его из очереди
+        :return:
+        """
         assert not self.empty(), 'Queue is empty!'
         return self.m_front.item
 
     def __len__(self):
+        """
+        переопределение (для длины len(Queue()))
+        :return:
+        """
         return self.size
+
+
+if __name__ == '__main__':
+    Q = Queue()
+    while True:
+        s = input()
+        if s == 'exit':
+            print('bye')
+            break
+        elif s == 'pop':
+            print(Q.pop())
+        elif s == 'front':
+            print(Q.front())
+        elif s == 'size':
+            print(len(Q))
+        elif s == 'clear':
+            while len(Q):
+                Q.pop()
+            print('ok')
+        else:
+            s = s.split()
+            Q.push(s[1])
+            print('ok')
